792534dfdccf36a43f54552120f67b7d613bb6b9
Leonardo Baldaia <leonardobaldaia@hotmail.com>
2024-04-05 11:52:00 -0300

    adcionando aulas

---
+++ aula 4/js/script.js
@@ -0,0 +1,8 @@
+/* alert("Olá, mundo!") // 
+prompt("digite seu nome")/* */
+var nome = prompt("digite seu nome")
+var idade = 23
+
+
+console.log("ola" + nome + ", seja bem vindo")
+console.log(`ola  ${nome}, seja bem vindo`)
